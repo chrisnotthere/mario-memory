2624315a80cc10cca5dcbaa338050cd92d71b33a
chris <mr.chrisnash@gmail.com>
2023-08-16 15:40:54 -0700

    basic game interaction tests

--- cypress/e2e/game.cy.ts
+++ cypress/e2e/game.cy.ts
@@ -1,8 +1,32 @@
-describe('Mario Memory Game', () => {
-  it('should load the game', () => {
-    cy.visit('http://localhost:3000/mario-memory');
-    cy.contains('0 moves').should('be.visible');
-    cy.get('button.controller-button').should('be.visible');
+describe("Game start and basic interactions", () => {
+  beforeEach(() => {
+    cy.visit("");
+  });
+
+  it("game should load", () => {
+    cy.contains("0 moves").should("be.visible");
+    cy.get("button.controller-button").should("be.visible");
+  });
+
+  it("clicking card starts timer and displays elapsed seconds", () => {
+    cy.get("div.card").first().click();
+    cy.get("div.timer").should("contain", "0");
+    cy.wait(1000);
+    cy.get("div.timer").should("contain", "1");
+  });
+
+  it("clicking card flips card and shows image", () => {
+    cy.get("div.card").first().should("not.have.descendants", "img.card-image");
+    cy.get("div.card").first().should("have.descendants", "img.card-block");
+    cy.get("div.card").first().click();
+    cy.get("div.card").first().should("have.descendants", "img.card-image");
+    cy.get("div.card").first().should("not.have.descendants", "img.card-block");
+  });
+
+  it("clicking two cards registers a move", () => {
+    cy.get("div.move-counter").should("contain", "0");
+    cy.get("div.card").eq(0).click();
+    cy.get("div.card").eq(1).click();
+    cy.get("div.move-counter").should("contain", "1");
   });
 });
-
