b4700a1a0b6564713f14e51511693b74b5e6058f
chris <mr.chrisnash@gmail.com>
2023-08-17 14:45:15 -0700

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { PORT, MONGODB_URI } from './config';
 import highScoresRouter from './routes/highScores';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI)
