3661fc35374113f2a74666c9a22b780ac7137e1a
chris <mr.chrisnash@gmail.com>
2023-08-18 14:50:44 -0700

    added highscore pagination, and improved styling

--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -13,10 +13,24 @@ function Scoreboard() {
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
   const [highScores, setHighScores] = useState<HighScore[] | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  // sort high scores by score
   const sortedHighScores = highScores
     ? highScores.slice().sort((a, b) => b.score - a.score)
     : [];
+
+  // pagination logic
+  const itemsPerPage = 6;
+  const totalPages = Math.ceil(
+    (highScores ? highScores.length : 0) / itemsPerPage
+  );
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentHighScores = sortedHighScores.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   // send game data to server
   const postHighscore = (name: string, score: number) => {
@@ -72,14 +86,33 @@ function Scoreboard() {
         <div>
           <h2>High Scores</h2>
           <div className="scores">
-            {sortedHighScores.map((item, index) => (
-              <div key={index} className="score-row">
-                <div className="rank-name">
-                  {index + 1} {item.name}
+            <div className="score-row-container">
+              {currentHighScores.map((item, index) => (
+                <div key={index} className="score-row">
+                  <div className="rank-name">
+                    {index + 1 + (currentPage - 1) * itemsPerPage} {item.name}
+                  </div>
+                  <div className="score">{item.score}</div>
                 </div>
-                <div className="score">{item.score}</div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="pagination-controls">
+              <button
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                Previous
+              </button>
+              <span>
+                {currentPage} of {totalPages}
+              </span>
+              <button
+                onClick={() => setCurrentPage(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       ) : (
