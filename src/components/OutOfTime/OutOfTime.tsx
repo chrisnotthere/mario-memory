646543c065ded0471dbea850d1733fc7c59bc11d
chris <mr.chrisnash@gmail.com>
2023-08-21 11:36:11 -0700

    out of time modal

---
+++ src/components/OutOfTime/OutOfTime.tsx
@@ -0,0 +1,11 @@
+import './outoftime.css';
+function OutOfTimeModal() {
+  return (
+    <div className="out-of-time-modal">
+      <h1>You Ran Out of Time!</h1>
+      <p>Click 'Reset' to try again</p>
+    </div>
+  );
+}
+
+export default OutOfTimeModal;
