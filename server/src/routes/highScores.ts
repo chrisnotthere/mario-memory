460ed3602402fb61fb60eb64f444a9c5263be85f
chris <mr.chrisnash@gmail.com>
2023-08-21 14:18:07 -0700

    highscore endpoints only return scores of the same difficulty. weekly-high-scores only returns scores from the past week

--- server/src/routes/highScores.ts
+++ server/src/routes/highScores.ts
@@ -1,15 +1,43 @@
-import express from 'express';
-import HighScore from '../models/highScore';
+import express from "express";
+import HighScore from "../models/highScore";
 
 const router = express.Router();
 
-router.get('/high-scores', (req, res) => {
-  HighScore.find({}).then((scores) => res.json(scores));
+router.get("/high-scores", (req, res) => {
+  const difficulty = req.query.difficulty;
+
+  HighScore.find({ difficulty })
+    .then((scores) => {
+      console.log("Found scores:", scores);
+      res.json(scores);
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
 });
 
-router.post('/high-scores', (req, res) => {
+router.get("/weekly-high-scores", (req, res) => {
+  const difficulty = req.query.difficulty;
+  const oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+  HighScore.find({
+    difficulty,
+    date: { $gte: oneWeekAgo.toISOString() },
+  })
+    .then((scores) => {
+      res.json(scores);
+    })
+    .catch((err) => {
+      console.error("Error fetching high scores:", err.message);
+      res.status(400).json({ error: err.message });
+    });
+});
+
+router.post("/high-scores", (req, res) => {
   const newScore = new HighScore(req.body);
-  newScore.save().then(() => res.json({ success: true }));
+  newScore
+    .save()
+    .then(() => res.json({ success: true }))
+    .catch((err) => res.status(400).json({ error: err.message }));
 });
 
 export default router;
