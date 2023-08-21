5b78ae9d18962a3242365666ac9d8be2eb15378f
chris <mr.chrisnash@gmail.com>
2023-08-21 13:21:21 -0700

    add date and difficulty to highScoreSchema

--- server/src/models/highScore.ts
+++ server/src/models/highScore.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const highScoreSchema = new mongoose.Schema({
   name: String,
   score: Number,
+  difficulty: String,
+  date: String,
 });
 
 const HighScore = mongoose.model('HighScore', highScoreSchema);
